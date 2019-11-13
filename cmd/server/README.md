3606b63ab30f86c4577adff2e9ba729a5bb19a4a
nikolas <panfuming@Nikolas-Mbp.local>
2019-11-13 11:21:48 +0800

    patch

--- cmd/server/README.md
+++ cmd/server/README.md
@@ -1,10 +1,10 @@
 # 启动主链节点
 
 ## 一、多节点配置(单节点无需更改，直接启动主链)
-[1、singleMode](https://github.com/PhenixChain/devchain/blob/master/cmd/server/main.toml#L35)
+[1、singleMode](https://github.com/PhenixChain/devchain/blob/master/cmd/server/main.toml#L34)
 修改为：singleMode=false
 
-[2、seeds](https://github.com/PhenixChain/devchain/blob/master/cmd/server/main.toml#L44)
+[2、seeds](https://github.com/PhenixChain/devchain/blob/master/cmd/server/main.toml#L43)
 修改为实际ip，例如：
 seeds=["192.168.0.1:13802","192.168.0.2:13802","192.168.0.3:13802"]
 
@@ -15,7 +15,7 @@ seeds=["192.168.0.1:13802","192.168.0.2:13802","192.168.0.3:13802"]
 如要部署在docker里运行：[参考](https://github.com/PhenixChain/devchain/tree/master/docker)
 
 ## 三、导入挖矿账户私钥，开启自动挖矿
-[开启挖矿](https://github.com/PhenixChain/devchain/blob/master/cmd/cli/README.md#L3)
+[开启挖矿](https://github.com/PhenixChain/devchain/blob/master/cmd/cli/README.md)
 
 
 备注：main.toml为主链配置文件；para.toml为平行链配置文件
--- plugin/dapp/init/init.go
+++ plugin/dapp/init/init.go
@@ -1,30 +1,14 @@
 package init
 
 import (
-	_ "github.com/PhenixChain/devchain/plugin/dapp/autonomy"   //auto gen
-	_ "github.com/PhenixChain/devchain/plugin/dapp/blackwhite" //auto gen
-	_ "github.com/PhenixChain/devchain/plugin/dapp/cert"       //auto gen
-
-	//_ "github.com/PhenixChain/devchain/plugin/dapp/dposvote"   //auto gen
-	//_ "github.com/PhenixChain/devchain/plugin/dapp/echo"       //auto gen
-	_ "github.com/PhenixChain/devchain/plugin/dapp/evm" //auto gen
-	//_ "github.com/PhenixChain/devchain/plugin/dapp/game"       //auto gen
-	//_ "github.com/PhenixChain/devchain/plugin/dapp/guess"      //auto gen
 	_ "github.com/PhenixChain/devchain/plugin/dapp/hashlock"  //auto gen
-	_ "github.com/PhenixChain/devchain/plugin/dapp/js"        //auto gen
-	_ "github.com/PhenixChain/devchain/plugin/dapp/lottery"   //auto gen
 	_ "github.com/PhenixChain/devchain/plugin/dapp/multisig"  //auto gen
-	_ "github.com/PhenixChain/devchain/plugin/dapp/norm"      //auto gen
-	_ "github.com/PhenixChain/devchain/plugin/dapp/oracle"    //auto gen
 	_ "github.com/PhenixChain/devchain/plugin/dapp/paracross" //auto gen
-
-	//_ "github.com/PhenixChain/devchain/plugin/dapp/pokerbull"  //auto gen
-	_ "github.com/PhenixChain/devchain/plugin/dapp/privacy"  //auto gen
-	_ "github.com/PhenixChain/devchain/plugin/dapp/relay"    //auto gen
-	_ "github.com/PhenixChain/devchain/plugin/dapp/retrieve" //auto gen
-	_ "github.com/PhenixChain/devchain/plugin/dapp/ticket"   //auto gen
-	_ "github.com/PhenixChain/devchain/plugin/dapp/token"    //auto gen
-	_ "github.com/PhenixChain/devchain/plugin/dapp/trade"    //auto gen
-	_ "github.com/PhenixChain/devchain/plugin/dapp/unfreeze" //auto gen
-	//_ "github.com/PhenixChain/devchain/plugin/dapp/valnode"  //auto gen
+	_ "github.com/PhenixChain/devchain/plugin/dapp/privacy"   //auto gen
+	_ "github.com/PhenixChain/devchain/plugin/dapp/relay"     //auto gen
+	_ "github.com/PhenixChain/devchain/plugin/dapp/retrieve"  //auto gen
+	_ "github.com/PhenixChain/devchain/plugin/dapp/ticket"    //auto gen
+	_ "github.com/PhenixChain/devchain/plugin/dapp/token"     //auto gen
+	_ "github.com/PhenixChain/devchain/plugin/dapp/trade"     //auto gen
+	_ "github.com/PhenixChain/devchain/plugin/dapp/unfreeze"  //auto gen
 )
